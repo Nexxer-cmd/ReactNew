9840513d398c2c81cd0a22b798ee0fb3cdbe25c6
Nexxer-cmd <oarjun790@gmail.com>
2025-09-30 08:05:47 +0530

    commit

---
+++ 02Vite/01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "Chai aur code"
+
+  return (
+    <>
+    <h3>Hello!! {username}</h3>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
